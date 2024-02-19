790ed4650db6b410359da5181506d5bdd902579a
vinod sjml <vinsjml@gmail.com>
2024-02-19 12:09:58 +0530

    push

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,12 @@ output "PUBLIC_ALB_ARN" {
 output "PRIVATE_ALB_ARN" {
   value = module.alb_private.ALB_ARN
 }
+
+output "PUBLIC_ALB_ADDRESS" {
+  value = module.alb_public.ALB_ADDRESS
+}
+
+output "PRIVATE_ALB_ADDRESS" {
+  value = module.alb_private.ALB_ADDRESS
+}
+
